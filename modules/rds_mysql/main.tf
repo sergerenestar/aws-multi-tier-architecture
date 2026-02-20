e6921731f324969df6d4897d1720be32b310cb78
Serge Rene <serge.rene.anaba.eteme.vz@gmail.com>
2026-02-20 10:41:06 -0500

    defined db module

---
+++ modules/rds_mysql/main.tf
@@ -0,0 +1,32 @@
+resource "aws_db_subnet_group" "this"{
+  name        = "${var.name}-db-subnets"
+  subnet_ids  = var.subnet_ids
+}
+
+resource "aws_db_instance" "mysql"{
+  #db instance type
+  identifier          = "${var.name}-mysql"
+  engine              = "mysgl"
+  engine_version      =8.0
+  instance_class      = var.instance_class
+  allocated_storage = 20
+
+#db config
+  db_name = var.db_name
+  username = var.db_username
+  password = var.db_password
+  port = 3306
+
+#db network confib
+  db_subnet_group_name  = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [var.db_sg_id]
+
+#db parameters 
+  publicly_accessible      = false
+  storage_encrypted        = true
+  backup_retention_period  = 7
+  skip_final_snapshot      = true
+  
+
+}
+
---
+++ modules/rds_mysql/outputs.tf
@@ -0,0 +1,4 @@
+output "db_host" {value = aws_db_instance.mysql.address}
+output "db_port" {value = aws_db_instance.mysql.port}
+output "db_name" {value = aws_db_instance.mysql.db_name}
+output "db_username" {value = aws_db_instance.mysql.username}
