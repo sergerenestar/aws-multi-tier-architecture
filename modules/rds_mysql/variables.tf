5110b14822a87d1dada8a1dcc194a8a95cfb36d4
Serge Rene <serge.rene.anaba.eteme.vz@gmail.com>
2026-02-20 09:56:58 -0500

    defined db module

---
+++ modules/rds_mysql/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {type = string}
+variable "vpc_id" {type =string}
+variable "subnet_ids" {type = list(string)}
+variable "db_sg_id" {type = string}
+
+variable "db_name" {type = string}
+variable "db_username" {type =string}
+variable "db_password" {
+  type = string
+  sensitive = true
+}
+
+variable "instance_class" {
+  type = string
+  default = "db.t3.micro"
+}
